2781843dc7b1c4e94517e3e290494af33b0a6e9e
James Hunter <jameshunterbr@gmail.com>
2024-10-10 18:06:29 -0300

    packages

---
+++ pacotes_iniciais.R
@@ -0,0 +1,11 @@
+packages <- c("tidyverse", "broom", "car", "caret", "corrr", "corrplot", 
+              "data.table", "DescTools", "devtools", "gapminder", "ggpubr", 
+              "ggsci", "glue", "gmodels", "gt", "gtsummary", "here", "Hmisc", 
+              "hms", "janitor", "jsonlite", "kableExtra","knitr", "lattice", 
+              "lubridate",  "nortest", "nycflights13", "paletteer", 
+              "patchwork", "plotly", "palmerpenguins", "pROC", "psych", 
+              "quarto", "Rcpp", "readxl", "reticulate", "ROCR", "shiny", 
+              "styler", "summarytools", "tidymodels", "titanic", "usethis", 
+              "DataExplorer")
+
+install.packages(packages)
